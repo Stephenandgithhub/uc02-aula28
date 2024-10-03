7483369efded34a2949dc1053f1f964277b634a6
Stephen <stephen.3942@aluno.pr.senac.br>
2024-10-02 21:53:34 -0300

    DOM manipulation

--- script.js
+++ script.js
@@ -1,20 +1,66 @@
 const contactData = {
-      "name": "HARPER STERN",
-      "email": "fulaninho@email.com",  
-      "phone": "46987654321",  
-      "cpf": "00123456789",  
-      "address": "Rua dos Bobos",  
-      "number": 0,  
-      "complemento": "Era uma casa muito engraçada",  
-      "cep": 123456789,  
-      "bairro": "Centro",  
-      "cidade": "Pato Branco",  
-      "estado": "Paraná",  
-      "sexo": "Masculino",  
-      "estadoCivil": "Divorciado",  
-      "profissao": "Programador",  
-      "dataNascimento": "01/01/1980"
+  name: "HARPER STERN",
+  email: "fulaninho@email.com",
+  phone: "46987654321",
+  cpf: "00123456789",
+  address: "Pierpoint Embankment",
+  number: 18,
+  complemento: "Era uma casa muito engraçada",
+  cep: 123456789,
+  bairro: "Centro",
+  cidade: "Pato Branco",
+  estado: "Paraná",
+  sexo: "Masculino",
+  estadoCivil: "Divorciado",
+  profissao: "Cross Product Sales (CPS)",
+  dataNascimento: "01/01/1980",
+  urls: ["https://linkedin.com/harperstern", "https://github.com/harperstern"],
+};
+
+//div title
+
+const divTitle = document.getElementById("title");
+
+const h1Name = document.createElement("h1");
+h1Name.textContent = contactData.name;
+
+divTitle.appendChild(h1Name);
+
+const spanProfissao = document.createElement("span");
+spanProfissao.textContent = contactData.profissao;
+
+divTitle.appendChild(spanProfissao);
+
+//div contact
+const divContact = document.createElement("div");
+const listContact = document.getElementById("contact");
+
+const listContacts = document.createElement("ul");
+
+//address list
+const itemAddress = document.createElement("li");
+itemAddress.textContent = `${contactData.address}, ${contactData.number}`;
+listContact.appendChild(itemAddress);
+
+//email list
+const itemEmail = document.createElement("li");
+itemEmail.textContent = `${contactData.email}`;
+listContact.appendChild(itemEmail);
+
+//phone
+const itemPhone = document.createElement("li");
+itemPhone.textContent = `${contactData.phone}`;
+listContact.appendChild(itemPhone);
+//links
+
+for (url of contactData.urls) {
+  const anchor = document.createElement("a");
+  anchor.textContent = url;
+  anchor.setAttribute("href", url);
+
+  const itemLink = document.createElement("li");
+  itemLink.appendChild(anchor);
+  listContact.appendChild(itemLink);
 }
-console.log(contactData)
-const h1name = document.getElementById("name")
-h1name.textContent = contactData.name
+
+divContact.appendChild(listContacts);
